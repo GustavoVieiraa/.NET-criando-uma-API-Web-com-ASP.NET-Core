b0cb46428498317d7614895e4b23ca4f2b210a6f
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-20 03:57:47 -0300

    feat: Separado as responsabilidades da nossa WebAPI, utilizando métodos de extensão, e estruturando nossos endponts em metodos estáticos, dentro da Pasta Endpoints.

---
+++ csharpWeb-screensound/ScreenSound.API/Endpoints/ArtistasExtensions.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScreenSound.Data;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.API.Endpoints
+{
+    public static class ArtistasExtensions
+    {
+        public static void AddEndpointsArtistas(this WebApplication app) {
+            #region Endpoint Artistas
+            app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) =>
+            {
+                return Results.Ok(dal.Listar());
+            });
+
+            app.MapGet("/Artistas/{nome}", (string nome) =>
+            {
+                var context = new ScreenSoundContext();
+                var artista = context.Artistas
+                        .Include(a => a.Musicas)
+                        .FirstOrDefault(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+
+                if (artista is null)
+                {
+                    return Results.NotFound();
+                }
+
+                return Results.Ok(artista);
+
+            });
+
+            app.MapPost("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista artista) => {
+                dal.Adicionar(artista);
+                return Results.Ok();
+            });
+
+            app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal, int id) =>
+            {
+                var artista = dal.RecuperarPor(a => a.Id == id);
+                if (artista is null)
+                {
+                    return Results.NotFound();
+                }
+
+                dal.Deletar(artista);
+
+                return Results.NoContent();
+
+            });
+
+            app.MapPut("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista artista) =>
+            {
+                var artistaParaAtualizar = dal.RecuperarPor(a => a.Id == artista.Id);
+
+                if (artistaParaAtualizar is null)
+                {
+                    return Results.NotFound();
+                }
+
+                artistaParaAtualizar.Nome = artista.Nome;
+                artistaParaAtualizar.Bio = artista.Bio;
+                artistaParaAtualizar.FotoPerfil = artista.FotoPerfil;
+
+                dal.Atualizar(artistaParaAtualizar);
+
+                return Results.Ok();
+
+            });
+            #endregion
+        }
+    }
+}
--- csharpWeb-screensound/ScreenSound.API/Program.cs
+++ csharpWeb-screensound/ScreenSound.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ScreenSound.API.Endpoints;
 using ScreenSound.Data;
 using ScreenSound.Modelos;
 using System.Text.Json.Serialization;
@@ -15,107 +16,8 @@ var app = builder.Build();
 
 app.MapGet("/", () => " API do Projeto ScreenSound está rodando!");
 
-app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) =>
-{
-    return Results.Ok(dal.Listar());
-});
+app.AddEndpointsArtistas();
 
-app.MapGet("/Artistas/{nome}", (string nome) =>
-{
-    var context = new ScreenSoundContext();
-    var artista = context.Artistas
-            .Include(a => a.Musicas) 
-            .FirstOrDefault(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
-
-    if (artista is null)
-    {
-        return Results.NotFound();
-    }
-
-    return Results.Ok(artista);
-
-});
-
-app.MapPost("/Artistas", ([FromServices] DAL < Artista > dal, [FromBody]Artista artista) => {
-    dal.Adicionar(artista);
-    return Results.Ok();
-});
-
-app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal, int id) =>
-{
-    var artista = dal.RecuperarPor(a => a.Id == id);
-    if (artista is null) {
-        return Results.NotFound();
-    }
-
-    dal.Deletar(artista);
-
-    return Results.NoContent();
-
-});
-
-app.MapPut("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody]Artista artista) =>
-{
-    var artistaParaAtualizar = dal.RecuperarPor(a => a.Id == artista.Id);
-
-    if (artistaParaAtualizar is null) {
-        return Results.NotFound();
-    }
-
-    artistaParaAtualizar.Nome = artista.Nome;
-    artistaParaAtualizar.Bio = artista.Bio;
-    artistaParaAtualizar.FotoPerfil = artista.FotoPerfil;
-
-    dal.Atualizar(artistaParaAtualizar);
-
-    return Results.Ok();
-
-});
-
-app.MapGet("/Musicas", ([FromServices] DAL<Musica> dal) =>
-{
-    return Results.Ok(dal.Listar());
-});
-
-app.MapGet("/Musicas/{nome}", ([FromServices] DAL<Musica> dal, string nome) =>
-{
-    var musica = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
-    if (musica is null)
-    {
-        return Results.NotFound();
-    }
-    return Results.Ok(musica);
-
-});
-
-app.MapPost("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) =>
-{
-    dal.Adicionar(musica);
-    return Results.Ok();
-});
-
-app.MapDelete("/Musicas/{id}", ([FromServices] DAL<Musica> dal, int id) => {
-    var musica = dal.RecuperarPor(a => a.Id == id);
-    if (musica is null)
-    {
-        return Results.NotFound();
-    }
-    dal.Deletar(musica);
-    return Results.NoContent();
-
-});
-
-app.MapPut("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) => {
-    var musicaAAtualizar = dal.RecuperarPor(a => a.Id == musica.Id);
-    if (musicaAAtualizar is null)
-    {
-        return Results.NotFound();
-    }
-    musicaAAtualizar.Nome = musica.Nome;
-    musicaAAtualizar.AnoLancamento = musica.AnoLancamento;
-
-    dal.Atualizar(musicaAAtualizar);
-    return Results.Ok();
-});
+app.AddEndpointsMusicas();
 
 app.Run();
