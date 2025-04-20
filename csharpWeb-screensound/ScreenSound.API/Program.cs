0d8c29754d26abfedf021ca8d55605c947c50a55
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-20 04:04:57 -0300

    feat: Implementado o Swagger para documentar a nossa API.

--- csharpWeb-screensound/ScreenSound.API/Program.cs
+++ csharpWeb-screensound/ScreenSound.API/Program.cs
@@ -11,13 +11,18 @@ builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
 builder.Services.AddTransient<DAL<Musica>>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
 app.MapGet("/", () => " API do Projeto ScreenSound está rodando!");
 
 app.AddEndpointsArtistas();
-
 app.AddEndpointsMusicas();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
