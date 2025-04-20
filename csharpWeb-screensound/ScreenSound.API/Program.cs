0b96774027692cb8fef3d99c841061ccff6b8105
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-20 02:44:36 -0300

    feat: Realizado a criação do endpoint para BuscarPor e aplicado StatusCode nas resposta. Também estrutura melhor o projeto.

--- csharpWeb-screensound/ScreenSound.API/Program.cs
+++ csharpWeb-screensound/ScreenSound.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using ScreenSound.Data;
 using ScreenSound.Modelos;
 using System.Text.Json.Serialization;
@@ -7,10 +8,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/", () =>
+app.MapGet("/", () => " API do Projeto ScreenSound está rodando!");
+
+app.MapGet("/Artistas", () =>
 {
     var dal = new DAL<Artista>(new ScreenSoundContext());
-    return dal.Listar();
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Artistas/{nome}", (string nome) =>
+{
+    var context = new ScreenSoundContext();
+    var artista = context.Artistas
+            .Include(a => a.Musicas) 
+            .FirstOrDefault(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+
+    if (artista is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(artista);
+
 });
 
 app.Run();
