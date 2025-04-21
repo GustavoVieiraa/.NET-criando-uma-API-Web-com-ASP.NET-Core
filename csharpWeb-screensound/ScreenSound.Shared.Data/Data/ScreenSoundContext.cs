abb2ba46d45d7905ff0fbaa58566fea4ada38171
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-21 00:23:07 -0300

    feat: Criado a Migration AddTableGenero e executado o Update-Database.

--- csharpWeb-screensound/ScreenSound.Shared.Data/Data/ScreenSoundContext.cs
+++ csharpWeb-screensound/ScreenSound.Shared.Data/Data/ScreenSoundContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ScreenSound.Modelos;
+using ScreenSound.Shared.Models.Models;
 
 namespace ScreenSound.Data
 {
@@ -8,6 +9,8 @@ namespace ScreenSound.Data
         public DbSet<Artista> Artistas { get; set; }
 
         public DbSet<Musica> Musicas { get; set; }
+
+        public DbSet<Genero> Generos { get; set; }
 
         private readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;TrustServerCertificate=False";
 
---
+++ csharpWeb-screensound/ScreenSound.Shared.Data/Migrations/20250421031941_AddTableGenero.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableGenero : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Idade",
+                table: "Artistas");
+
+            migrationBuilder.CreateTable(
+                name: "Generos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Generos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Generos");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Idade",
+                table: "Artistas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
